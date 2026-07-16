37c32401e24f6e3fbaa34906454c666308815533
Ibnballo1 <belloabdullateef035@gmail.com>
2026-07-16 11:12:44 +0100

    update next config

--- apps/libredebt-web/next.config.ts
+++ apps/libredebt-web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   // 1. Tell Next.js 16 we acknowledge Turbopack is active (this silences the error)
   turbopack: {},
 };
